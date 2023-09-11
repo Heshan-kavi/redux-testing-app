24d4ec84e712ae2cc9b1b476f9badb1b744bc307
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-11 14:36:43 +0530

    open modal and the backdrop when the user clicked delete button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Backdrop from './components/Backdrop';
-import Modal from './components/Modal';
 import Todo from './components/Todo';
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
       <Todo text="Take a cup"/>
       <Todo text="Wash the cup"/>
       <Todo text="Add milk to the cup"/>
-      <Modal/>
-      <Backdrop/>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props){
 
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     function deletehandler(){
-        console.log(props.text);
+        setModalIsOpen(true);
     }
 
     return (
         <div className='card'>
-            <h2 className='actions'>{props.text}</h2>
-            <button className='btn' onClick={deletehandler}>delete</button>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deletehandler}>Delete</button>
+            </div>
+            {modalIsOpen && <Modal/>}
+            {modalIsOpen && <Backdrop/>}
         </div>
     );
 }
