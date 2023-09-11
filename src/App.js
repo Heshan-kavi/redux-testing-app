0846e0c901a78845196f8846f8ae17e366980dbc
Hesh-Kavinda <heshan@ascnetic.se>
2023-09-11 13:10:02 +0530

    Added new todocomponent for the reusable purpose

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
       <h1>To Do List</h1>
-      <div className='card'>
-        <h2 className='actions'>TITLE</h2>
-        <button className='btn'>delete</button>
-      </div>
+      <Todo/>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+function Todo(){
+    return (
+        <div className='card'>
+            <h2 className='actions'>TITLE</h2>
+            <button className='btn'>delete</button>
+        </div>
+    );
+}
+
+export default Todo;
